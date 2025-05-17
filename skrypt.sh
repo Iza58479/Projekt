d4016bdc612d6a477abe8dfac9eade6bf5a83720
Iza <dsw58479@student.dsw.edu.pl>
2025-05-18 00:49:00 +0200

    Dodano obsługę opcji --help

--- skrypt.sh
+++ skrypt.sh
@@ -6,21 +6,29 @@ case $1 in
         ;;
 
     --logs)
-    if [ -n "$2" ] && [[ "$2" =~ ^[0-9]+$ ]]; then
-      num_files="$2"
-      for i in $(seq 1 "$num_files"); do
-        filename="log${i}.txt"
-        echo -e "Nazwa pliku: $filename\nSkrypt: $0\nData: $(date)" > "$filename"
-      done
-      echo "Utworzono $num_files plików log."
-    else
-        for i in $(seq 1 100); do
+        if [ -n "$2" ] && [[ "$2" =~ ^[0-9]+$ ]]; then
+            num_files="$2"
+        else
+            num_files=100
+        fi
+
+        for i in $(seq 1 "$num_files"); do
             filename="log${i}.txt"
-            echo -e "Nazwa pliku: $filename\nSkrypt: $0\nData: $(date)" > $filename
-	    echo "Utworzono 100 plików."
+            echo -e "Nazwa pliku: $filename\nSkrypt: $0\nData: $(date)" > "$filename"
         done
-fi
-    ;;
+        echo "Utworzono $num_files plików log."
+        ;;
+
+    --help)
+        echo "Dostępne opcje:"
+        echo "  --date      Wyświetla dzisiejszą datę"
+        echo "  --logs [N]  Tworzy N plików logx.txt (domyślnie 100)"
+        echo "  --help      Wyświetla ten tekst pomocy"
+        ;;
+        
+    *)
+        echo "Nieznana opcja. Użyj --help aby zobaczyć dostępne opcje."
+        ;;
 esac
 
 exit 0
