0745dda97dd744738b7cf23a5458ad27cd59a236
Iza <dsw58479@student.dsw.edu.pl>
2025-05-18 00:23:28 +0200

    Zaktualizowano obsługę opcji --logs o argument liczby plików

--- skrypt.sh
+++ skrypt.sh
@@ -6,9 +6,21 @@ case $1 in
         ;;
 
     --logs)
+    if [ -n "$2" ] && [[ "$2" =~ ^[0-9]+$ ]]; then
+      num_files="$2"
+      for i in $(seq 1 "$num_files"); do
+        filename="log${i}.txt"
+        echo -e "Nazwa pliku: $filename\nSkrypt: $0\nData: $(date)" > "$filename"
+      done
+      echo "Utworzono $num_files plików log."
+    else
         for i in $(seq 1 100); do
             filename="log${i}.txt"
             echo -e "Nazwa pliku: $filename\nSkrypt: $0\nData: $(date)" > $filename
+	    echo "Utworzono 100 plików."
         done
-        ;;
+fi
+    ;;
 esac
+
+exit 0
